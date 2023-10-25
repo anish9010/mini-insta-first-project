899e4ee743e3d7dc4262f9f85f391dd6b13da5a1
guriyakumari <guriyakumarigupta551@gmail.com>
2023-10-26 00:26:25 +0530

    first anish commit

---
+++ routes/post.js
@@ -0,0 +1,18 @@
+import express from 'express';
+const router = express.Router();
+
+//importing controllers
+import {createPost,getAllPost,getMyPost,incLike,disLike,insertComment,deletePost,getAllFollowingPost} from '../controllers/post';
+import {requireSignin} from '../middleware/index';
+
+
+router.get('/allPost',requireSignin,getAllPost);
+router.get('/allFollowingPost',requireSignin,getAllFollowingPost);
+router.get('/myPost',requireSignin,getMyPost);
+router.post('/createPost',requireSignin,createPost);
+router.put('/like',requireSignin,incLike);
+router.put('/dislike',requireSignin,disLike);
+router.put('/comment',requireSignin,insertComment);
+router.delete('/deletePost/:postId',requireSignin,deletePost);
+
+module.exports = router;
